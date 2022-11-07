14f84ae54a7bccc01d80559b5e1a53002b682853
andguilherme <andersongui@gmail.com>
2022-11-07 11:35:51 -0300

    shell script para provisionar servidor apache

---
+++ projeto_apache.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+
+echo "baixando apache..."
+
+apt update
+apt upgrade -y
+apt install apache 2 -y
+apt install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
